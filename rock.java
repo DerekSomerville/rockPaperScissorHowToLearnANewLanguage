f81cf73714cf142c4587a7bae68d850acafe9686
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2019-11-26 19:37:34 +0000

    Update

--- rock.java
+++ rock.java
@@ -4,43 +4,80 @@ import java.util.Random;
 
 class RockPaperScissor {
 
-	static void requestPlay(String[] weapons){
-		String display = "Please select";
-		/*for (int i=0;i<weapons.length;i++){*/
-		int i=0;
-		for (String weapon: weapons){
-			i++;
-			display += " " + i + ". " + weapon;
-		};
-		System.out.println(display);
-	}
-	
-	public static void main(String[ ] args) {
-		RockPaperScissor rockPaperScissor = new RockPaperScissor();
-		rockPaperScissor.run();
-	}
-	public void run(){
-		int userWeapon;
-		/*BufferedReader userInput = 
-		 new BufferedReader( new InputStreamReader(System.in));*/
-		 
-		Scanner userInput = new Scanner(System.in);
-		Random rand = new Random();
-		int computerWeapon;
-		//Final declares a constant
-		final String[] weaponList = {"Rock","Scissors","Paper"};
-		do{
-			requestPlay(weaponList);
-	  		userWeapon = userInput.nextInt()-1;
-	  		computerWeapon = rand.nextInt(3);
-			System.out.println("Users selected "+ weaponList[userWeapon] + " Computer selected " + weaponList[computerWeapon]);
-			if (userWeapon == computerWeapon){
-				System.out.println("Draw");
-			} else if ((userWeapon + 1)%3 == computerWeapon){
-				System.out.println("You won");
-			} else if ((computerWeapon+1)%3 == userWeapon) {
-				System.out.println("Computer won");
-			}
-		} while (userWeapon< 4);
-	}
+        static String generateRequest(String[] weapons){
+            String display = "Please select";
+            /*for (int i=0;i<weapons.length;i++){*/
+            int i=0;
+            for (String weapon: weapons){
+                i++;
+                display += " " + i + " " + weapon;
+            };
+            return display;
+        }
+
+        static int requestPlay(String[] weapons)
+        {
+            String request;
+            Scanner userInput = new Scanner(System.in);
+            request = generateRequest(weapons);
+            System.out.println(request);
+
+            int userWeapon = userInput.nextInt()-1;
+
+            return userWeapon;
+        }
+
+        public static void main(String[ ] args) {
+            RockPaperScissor rockPaperScissor = new RockPaperScissor();
+            rockPaperScissor.run();
+        }
+
+        public static String determineWinner(String[] weaponList, int userWeapon, int computerWeapon)
+        {
+            String winner;
+            if (userWeapon == computerWeapon)
+            {
+                winner = "Draw both selected " + weaponList[computerWeapon];
+            }
+            else if ((userWeapon + 1) % 3 == computerWeapon)
+            {
+                winner = "You win and beat the computer's " + weaponList[computerWeapon];
+            }
+            else if ((computerWeapon + 1) % 3 == userWeapon)
+            {
+                winner = "Computer wins with " + weaponList[computerWeapon];
+            }
+            else
+            {
+                winner = "Please select 1. Rock, 2. Scissors or 3. Paper";
+            }
+
+            return winner;
+
+        }
+
+        private static void displayWinner(String winner)
+        {
+            System.out.println(winner);
+        }
+
+    public void run(){
+            int userWeapon;
+		    /*BufferedReader userInput =
+		    new BufferedReader( new InputStreamReader(System.in));*/
+
+
+            Random rand = new Random();
+            int computerWeapon;
+            computerWeapon = rand.nextInt(3);
+            String winner;
+            //Final declares
+            final String[] weaponList = {"Rock","Scissors","Paper"};
+            do{
+                userWeapon = requestPlay(weaponList);
+
+                winner = determineWinner(weaponList, userWeapon, computerWeapon);
+                displayWinner(winner);
+            } while (userWeapon< 3);
+        }
 }
